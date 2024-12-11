f00c7feb9f5f02e257d4c2c5da98306d57073289
Your Name01~ <you@example.com>
2024-12-11 15:30:44 +0700

    display infor

--- src/pages/book.tsx
+++ src/pages/book.tsx
@@ -1,14 +1,127 @@
 import React from "react";
 import SliderBook from "./book/sliderBook";
+import Text from "./book/text";
 const App: React.FC = () => {
-  return (
-    <div className="flex">
-      <div className="max-w-[400px]">
-        <SliderBook></SliderBook>
-      </div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+    return (
+        <div className="flex items-start bg-[#F5F5FA] py-4 gap-5 justify-center">
+            {/* cot 1 */}
+            <div className="max-w-[400px] rounded-lg p-4 bg-white">
+                <SliderBook></SliderBook>
+            </div>
+            {/* cot 2 */}
+            <div className="flex flex-col gap-5 w-1/3">
+                <div className="bg-white rounded-lg p-3 flex flex-col gap-3">
+                    <p>
+                        Tác giả:{" "}
+                        <span className="text-[rgb(13,_92,_182)]">
+                            John C.Maxwell
+                        </span>
+                    </p>
+                    <h3 className="text-[rgb(39,_39,_42)] text-xl font-medium">
+                        Thay Đổi Tư Duy - 11 Phương Pháp Thay Đổi Thói Quen Để
+                        Sống Đời Thành Công (John C. Maxwell)
+                    </h3>
+                    <p className="text-[rgb(120,_120,_120)] text-sm">
+                        Đã bán 1
+                    </p>
+                    <div className="flex items-center gap-2">
+                        <span className="text-2xl text-[rgb(255,_66,_78)] font-semibold">
+                            76000₫
+                        </span>
+                        <span className="rounded-lg bg-[rgb(245,_245,_250)] font-normal text-xs px-1">
+                            -23%
+                        </span>
+                    </div>
+                </div>
+                <div className="bg-white rounded-lg p-3">
+                    <span className="font-semibold">Thông tin vận chuyển</span>
+                    <p>Giao đến Q. Hoàn Kiếm, P. Hàng Trống, Hà Nội</p>
+                </div>
+                <div className="bg-white rounded-lg p-3">
+                    <span className="font-semibold">Ưu đãi khác</span>
+                    <p>5 Mã giảm giá</p>
+                </div>
+                <div className="bg-white rounded-lg p-3 gap-3 flex flex-col">
+                    <span className="font-semibold">Thông tin chi tiết</span>
+                    <div className="flex items-center justify-between border-solid border-b-1 pb-2">
+                        <span className="text-[rgb(128,_128,_137)]">
+                            Công ty phát hành
+                        </span>
+                        <span>Alpha books</span>
+                    </div>
+                    <div className="flex items-center justify-between border-solid border-b-1 pb-2">
+                        <span className="text-[rgb(128,_128,_137)]">
+                            Ngày xuất bản
+                        </span>
+                        <span>2024-08-13 10:10:41</span>
+                    </div>
+                    <div className="flex items-center justify-between border-solid border-b-1 pb-2">
+                        <span className="text-[rgb(128,_128,_137)]">
+                            Kích thước
+                        </span>
+                        <span>15 x 15 cm</span>
+                    </div>
+                    <div className="flex items-center justify-between border-solid border-b-1 pb-2">
+                        <span className="text-[rgb(128,_128,_137)]">
+                            Loại bìa
+                        </span>
+                        <span>Bìa mềm</span>
+                    </div>
+                    <div className="flex items-center justify-between border-solid border-b-1 pb-2">
+                        <span className="text-[rgb(128,_128,_137)]">
+                            Số trang
+                        </span>
+                        <span>208</span>
+                    </div>
+                    <div className="flex items-center justify-between pb-2">
+                        <span className="text-[rgb(128,_128,_137)]">
+                            Nhà xuất bản
+                        </span>
+                        <span>Nhà Xuất Bản Công Thương</span>
+                    </div>
+                </div>
+                <div className="bg-white rounded-lg p-3">
+                    <span className="font-semibold">Mô tả sản phẩm</span>
+                    <div className="h-[250px] overflow-hidden py-4 relative">
+                        <Text></Text>
+                        <div className="h-[200px] absolute w-full bottom-0 left-0 bg-[linear-gradient(rgba(255,_255,_255,_0),_rgb(255,_255,_255))]"></div>
+                    </div>
+                    <a className="block w-full text-[rgb(24,_158,_255)] text-center" href="#!">Xem thêm</a>
+                </div>
+            </div>
+            {/* cot 3 */}
+            <div className="bg-white rounded-lg p-3 flex flex-col gap-3 w-[360px]">
+                <span className="font-semibold">Số lượng</span>
+                <div className="flex items-center gap-2">
+                    <button className="rounded cursor-pointer border-1 border-solid h-[32px] px-1">
+                        <img
+                            src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/icons-remove.svg"
+                            alt=""
+                        />
+                    </button>
+                    <input
+                        type="text"
+                        className="outline-none rounded border-1 border-solid p-1 text-center h-[32px] px-1 w-[40px]"
+                    />
+                    <button className="rounded cursor-pointer border-1 border-solid p-1 h-[32px] px-1">
+                        <img
+                            src="https://frontend.tikicdn.com/_desktop-next/static/img/pdp_revamp_v2/icons-add.svg"
+                            alt=""
+                        />
+                    </button>
+                </div>
+                <span className="font-semibold">Tạm tính</span>
+                <span className="font-semibold text-2xl">76000₫</span>
+                <div className="flex flex-col gap-2">
+                    <button className="bg-[rgb(255,-66,_78)] rounded cursor-pointer p-2 text-[rgb(255,_255,_255)]">
+                        Mua ngay
+                    </button>
+                    <button className="rounded cursor-pointer p-2 text-[rgb(10,_104,_255)] border-1 border-solid border-[rgb(10,_104,_255)]">
+                        Thêm vào giỏ
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default App;
---
+++ src/pages/book/text.tsx
@@ -0,0 +1,103 @@
+const App : React.FC = () => {
+    return <p className="whitespace-pre-wrap">Bộ sách Maxwell Moments
+
+    THAY ĐỔI TƯ DUY
+    
+    11 phương pháp thay đổi thói quen để sống đời thành công
+    
+    A. NỘI DUNG CUỐN SÁCH
+    
+    Dù bạn đang cạnh tranh với chính mình hay với người khác, thì việc suy nghĩ đúng đắn chính là chìa khóa. Trong "The Thinker's Edge", tác giả bán chạy quốc tế và chuyên gia lãnh đạo John C. Maxwell chia sẻ 11 phương pháp đơn giản để giúp bạn suy nghĩ hiệu quả hơn và trở thành phiên bản tốt hơn của chính mình, tạo lợi thế trong cả công việc lẫn cuộc sống. Bằng cách phát triển tư duy và góc nhìn sáng tạo, bạn sẽ trở nên sáng tạo và linh hoạt hơn trong chiến lược, đồng thời biết tiếp thu và đánh giá cao người khác. Hãy làm theo lời khuyên của Maxwell, và bạn cùng đội ngũ của mình sẽ đạt được thành tích vượt trội.
+    
+    - Thực hành 1: Suy nghĩ tích cực: Phát triển một tư duy tích cực để nhìn nhận và giải quyết vấn đề một cách lạc quan.
+    - Thực hành 2: Tư duy chiến lược: Lên kế hoạch và định hướng rõ ràng cho các mục tiêu dài hạn.
+    - Thực hành 3: Sáng tạo: Khuyến khích sự sáng tạo và tìm ra các giải pháp mới cho các vấn đề cũ.
+    - Thực hành 4: Phân tích: Đánh giá thông tin và dữ liệu một cách khách quan để đưa ra quyết định đúng đắn.
+    - Thực hành 5: Linh hoạt: Thích ứng nhanh chóng với các thay đổi và tình huống mới.
+    - Thực hành 6: Kiên nhẫn: Phát triển sự kiên nhẫn để đạt được thành công lâu dài.
+    - Thực hành 7: Học hỏi không ngừng: Luôn học hỏi và cập nhật kiến thức mới để duy trì sự cạnh tranh.
+    - Thực hành 8: Kết nối: Xây dựng và duy trì mối quan hệ chất lượng với người khác.
+    - Thực hành 9: Tư duy phê phán: Luôn đặt câu hỏi và thách thức các giả định để hiểu sâu hơn.
+    - Thực hành 10: Tập trung: Duy trì sự tập trung vào những mục tiêu quan trọng nhất.
+    - Thực hành 11: Cảm xúc: Quản lý cảm xúc để giữ vững tinh thần và động lực.
+     
+    
+    B. CUỐN SÁCH NÀY DÀNH CHO AI?
+    
+    - Người mới bắt đầu sự nghiệp, nhân viên văn phòng tìm cách thích nghi, cải thiện hiệu suất và tiến xa trong công việc.
+    
+    - Nhà quản lý và lãnh đạo muốn nâng cao kỹ năng tư duy và chiến lược. 
+    
+    - Chuyên gia và doanh nhân nuôi dưỡng tư duy sáng tạo và duy trì lợi thế cạnh tranh.
+    
+     
+    
+    C. CUỐN SÁCH NÀY CÓ GÌ ĐẶC BIỆT?
+    
+    - Hành trình khám phá bản thân, giúp bạn nhận ra những tiềm năng ẩn giấu bên trong mình qua 7 phương pháp cụ thể và dễ dàng áp dụng vào cuộc sống hàng ngày, là những bước đi chi tiết, giúp bạn từng bước khám phá và khai thác tiềm năng của mình.
+    
+    - Một hướng dẫn thực tế giúp bạn khai thác tối đa tiềm năng tư duy của mình. 
+    
+    - Bạn sẽ Hiểu rõ hơn về sức mạnh của tư duy; Phát triển tư duy sáng tạo và đổi mới; Tăng cường khả năng giải quyết vấn đề; Xây dựng tư duy chiến lược; Cải thiện kỹ năng ra quyết định; Tạo dựng tư duy tích cực và lạc quan.
+    
+    - Nội dung ngắn gọn, dễ hiểu, phù hợp với những người bận rộn.
+    
+    - Các bài học thực tế, có thể áp dụng ngay vào cuộc sống.
+    
+    - Lời khuyên thiết thực từ tác giả John C. Maxwell - tác giả bán chạy quốc tế và chuyên gia lãnh đạo được công nhận toàn cầu.
+    
+    - Điểm độc đáo nằm ở cách trình bày và truyền đạt. Mỗi trang sách là một thông điệp ngắn gọn và rõ ràng. Không có những câu chuyện dài và câu từ hoa mỹ, tác giả đánh trúng vào trọng tâm những câu hỏi đọc giả có thể đặt ra và cho họ lời khuyên đúng đắn, tích cực.
+     
+    
+    D. ĐÔI NÉT VỀ TÁC GIẢ 
+    
+    John C. Maxwell là tác giả, huấn luyện viên và diễn giả có sách bán chạy nhất của tờ New York Times. Sách của ông đã được dịch ra 50 ngôn ngữ và bán được hơn 24 triệu bản.
+    
+    Thường được gọi là chuyên gia lãnh đạo số 1 của Hoa Kỳ, Maxwell được tạp chí Inc. công nhận là chuyên gia lãnh đạo nổi tiếng nhất thế giới vào năm 2014; được bình chọn là chuyên gia lãnh đạo hàng đầu trong sáu năm liên tiếp trên LeadershipG
+    
+    Ông là người sáng lập The John Maxwell Company, The John Maxwell Team và EQUIP, một tổ chức phi lợi nhuận đã đào tạo hơn 5 triệu nhà lãnh đạo tại 180 quốc gia. Hằng năm, Maxwell đều nói chuyện với các công ty thuộc Fortune 500, các tổng thống của các quốc gia và nhiều nhà lãnh đạo doanh nghiệp hàng đầu thế giới.
+    
+     
+    
+    E. VỀ BỘ SÁCH MAXWELL MOMENTS  
+    
+    Dòng sách Maxwell Moments là một bộ sưu tập sách mới đầy sáng tạo, khác biệt hoàn toàn so với các cuốn sách khác của tác giả John C. Maxwell trên thị trường. Với diện mạo và cảm giác mới mẻ, bộ sách này không chỉ đơn thuần là một tập hợp những câu chuyện truyền cảm hứng, mà còn là một cẩm nang thực tế với những bài học sâu sắc về lãnh đạo, phát triển bản thân và đạt được mục tiêu.
+    
+    Qua mỗi câu chuyện ngắn gọn, John Maxwell chia sẻ những khoảnh khắc quan trọng (Maxwell Moments) trong cuộc sống, những khoảnh khắc có thể thay đổi hoàn toàn hướng đi của một con người.
+    
+    Tại sao nên đọc bộ sách Maxwell Moments?
+    
+    - Những bài học thiết thực: Bộ sách cung cấp những nguyên tắc và kỹ năng cụ thể mà bạn có thể áp dụng ngay vào cuộc sống hàng ngày, lấy cảm hứng từ những câu chuyện thực tế của những người thành công, những người đã vượt qua khó khăn và đạt được những thành tựu đáng nể.
+    - Phát triển tư duy tích cực: Bộ sách giúp bạn nuôi dưỡng một tư duy tích cực, lạc quan và tin vào bản thân.
+    - Cải thiện kỹ năng lãnh đạo: Dù bạn có phải là một nhà lãnh đạo hay không, những bài học về lãnh đạo trong sách sẽ giúp bạn trở thành một người có ảnh hưởng tích cực đến những người xung quanh.
+    - Đạt được mục tiêu cá nhân: Bộ sách cung cấp những công cụ và chiến lược để giúp bạn xác định mục tiêu và đạt được chúng.
+    
+     
+    
+    TRÍCH DẪN HAY TỪ CUỐN SÁCH 
+    
+    "HỌC HỎI TỪ MỌI TRẢI NGHIỆM
+    Đừng ngủ quên trên thành công. Đừng tập trung vào chúng. Thay vào đó, hãy học từ chúng. Và quan trọng hơn, hãy học từ những thất bại mình phạm phải. Khả năng chấp nhận được dạy dỗ là chìa khóa để nhìn được rộng hơn, sâu hơn và xa hơn.
+    Bạn càng có nhiều trải nghiệm đa dạng trong cuộc sống thì càng có nhiều tiềm năng học hỏi. Hãy thử thật nhiều thứ. Hãy nắm bắt thật nhiều cơ hội. Hãy khám phá.
+    Sau mỗi trải nghiệm, hãy dành thời gian để suy ngẫm. Kinh nghiệm không phải là người thầy tốt nhất – kinh nghiệm được đánh giá mới là người thầy tốt nhất.”
+    
+    
+    "LÀM QUEN VỚI SỰ BẤT TIỆN
+    Đi theo đám đông thật tiện. Ở cạnh những người đồng tình với mình thật tiện. Nó giống như ngồi trên một chiếc ghế tựa cũ đã được điều chỉnh cho phù hợp với mọi đặc điểm riêng của bạn.
+    Suy nghĩ khác với những người khác lại không hề tiện. Hãy làm quen với nó.”
+    “Sau mỗi trải nghiệm, hãy dành thời gian để suy ngẫm. Kinh nghiệm không phải là người thầy tốt nhất – kinh nghiệm được đánh giá mới là người thầy tốt nhất.”
+    “Lần tới khi bạn chuẩn bị đi theo lối suy nghĩ phổ biến về một vấn đề nào đó, hãy chọn đừng tuân theo một cách mù quáng.
+    Ngừng lại.
+    Suy nghĩ.
+    Đặt câu hỏi.
+    Còn cách nào tốt hơn không? Hãy đi theo hướng đó, bất kể người khác có đang làm gì. Hãy cứ để họ bảo là bạn sai rồi. Hãy cứ để họ gọi bạn là đồ ngốc. Hãy cứ để họ nói về những việc họ sẽ làm. Hãy trở nên không phổ biến. Nhưng hãy đến nơi bạn cần đến để làm điều đúng đắn và thành công.”
+    
+    
+    “ĐỪNG ĐA NHIỆM
+    Do nhịp độ hối hả trong văn hóa của chúng ta, bạn có thể muốn làm nhiều việc cùng một lúc. Đừng làm thế. Việc chuyển từ nhiệm vụ này sang nhiệm vụ khác có thể khiến bạn mất tới 40% hiệu suất. Nó phá vỡ sự tập trung của bạn.”
+    “Các nghiên cứu về tính sáng tạo cho thấy biến số lớn nhất quyết định liệu nhân viên có sáng tạo hay không là ở chỗ họ có nhận thấy mình được phép hay không.” – David Hills
+    
+     
+    Giá sản phẩm trên Tiki đã bao gồm thuế theo luật hiện hành. Bên cạnh đó, tuỳ vào loại sản phẩm, hình thức và địa chỉ giao hàng mà có thể phát sinh thêm chi phí khác như phí vận chuyển, phụ phí hàng cồng kềnh, thuế nhập khẩu (đối với đơn hàng giao từ nước ngoài có giá trị trên 1 triệu đồng).....</p>
+}
+export default App;
